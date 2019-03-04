8fe159fcbf53eba42f211a042afb0417c0a573f2
Niko <Nirailay@gmail.com>
2019-03-04 10:14:35 +0100

    TDD 3/6

--- starwars/src/index.test.js
+++ starwars/src/index.test.js
@@ -1,29 +1,37 @@
 const starWars = require('./index');
 const starWarsNames = require('./starwars-names.json');
 
+
+
 describe('starwars-names', () => {
   describe('all', () => {
     test('should be a fulfilled array', () => {
       expect(starWars.all).toHaveLength(starWarsNames.length)
     });
+	
+	
     test('should be an array of strings', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      expect(starWarsNames).ToBe(String);
     });
+	
 
     test('should contain `Luke Skywalker`', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      expect(starWarsNames).toContain('Luke Skywalker');
     });
+	
 
     test('should not contain `Ben Quadinaros`', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      expect(starWarsNames).not.toContain('Ben Quadinaros');
     });
   });
 
   describe('random', () => {
     test('should return a random item from the starWars.all', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+		const rand = random(2);
+      expect(rand).toHaveReturned();
     });
 
+	
     test('should return an array of random items if passed a number', () => {
       throw new Error('test not yet defined... remove the throw and write your test here');
     });
